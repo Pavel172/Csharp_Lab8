9091c12c0df1b3e28fcf6863b2d383e344606222
Павел Лустин <144838661+Pavel172@users.noreply.github.com>
2024-11-24 17:30:32 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Reflection.Emit;
+using System.Diagnostics.Metrics;
+
+namespace C__LAB8_LIBRARY
+{
+    public class MyAttribute : Attribute
+    {
+        public string Comment { get; set; }
+        public MyAttribute() { }
+        public MyAttribute(string Comment)
+        {
+            this.Comment = Comment;
+        }
+    }
+
+    [MyAttribute("Базовый класс Animal для всех животных")]
+    [XmlInclude(typeof(Cow))]
+    public abstract class Animal
+    {
+        public string Country { get; set; }
+        public string HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public string WhatAnimal { get; set; }
+        public string classification { get; set; }
+        public string favourite_food { get; set; }
+
+        public Animal() 
+        {
+            Country = "";
+            HideFromOtherAnimals = "";
+            Name = "";
+            WhatAnimal = "Cow";
+            classification = null;
+            favourite_food = null;
+        }
+        public Animal(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food)
+        {
+            this.Country = Country;
+            this.HideFromOtherAnimals = HideFromOtherAnimals;
+            this.Name = Name;
+            this.WhatAnimal = WhatAnimal;
+            this.classification = classification.ToString();
+            this.favourite_food = favourite_food.ToString();
+        }
+
+        public void Deconstruct(out string country, out string hideFromOtherAnimals, out string name, out string whatAnimal)
+        {
+            country = Country;
+            hideFromOtherAnimals = HideFromOtherAnimals;
+            name = Name;
+            whatAnimal = WhatAnimal;
+        }
+
+        public virtual void GetClassificationAnimal() { }
+        public virtual void GetFavouriteFood() { }
+        public virtual void SayHello() { }
+
+    }
+
+
+    [MyAttribute("Производный от Animal класс Cow")]
+    public class Cow : Animal
+    {
+        public Cow() : base()
+        {
+        }
+        public Cow(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("moooooo");
+        }
+    }
+
+
+    [MyAttribute("Производный от Animal класс Lion")]
+    public class Lion : Animal
+    {
+        public Lion(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("grrrrrrr");
+        }
+    }
+
+
+    [MyAttribute("Производный от Animal класс Pig")]
+    public class Pig : Animal
+    {
+        public Pig(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classification, eFavouriteFood favourite_food) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classification, favourite_food) { }
+        public override void GetClassificationAnimal()
+        {
+            Console.WriteLine($"{this.Name} классифицируется как {classification}");
+        }
+        public override void GetFavouriteFood()
+        {
+            Console.WriteLine($"У {this.Name} любимая еда - это {favourite_food}");
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine("oink-oink");
+        }
+    }
+
+
+    [MyAttribute("Перечисление классификаций животных")]
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+
+    [MyAttribute("Перечисление любимой еды животных")]
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using C__LAB8_LIBRARY;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace lAB8_serialization
+{
+    class Program
+    {
+        static void Main()
+        {
+            //ЗАДАНИЕ1
+            Animal cow = new Cow("Russia", "No", "Bessie", "Cow", eClassificationAnimal.Herbivores, eFavouriteFood.Plants);
+            XmlSerializer serializer = new XmlSerializer(typeof(Animal)); //реализация сериализации
+            using (TextWriter writer = new StreamWriter("animal.xml"))
+            {
+                serializer.Serialize(writer, cow);
+            }
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(Animal)); //реализация десериализации
+            using (TextReader reader = new StreamReader("animal.xml"))
+            {
+                Animal deserializedAnimal = (Animal)deserializer.Deserialize(reader);
+                Console.WriteLine($"Deserialized Animal:");
+                Console.WriteLine($"Country: {deserializedAnimal.Country}");
+                Console.WriteLine($"Hide From Other Animals: {deserializedAnimal.HideFromOtherAnimals}");
+                Console.WriteLine($"Name: {deserializedAnimal.Name}");
+                Console.WriteLine($"What Animal: {deserializedAnimal.WhatAnimal}");
+                Console.WriteLine($"Classification: {deserializedAnimal.classification}");
+                Console.WriteLine($"Favourite Food: {deserializedAnimal.favourite_food}");
+                
+                //ЗАДАНИЕ2
+                string filePath = @"C:\Users\Pavel\source\repos\C#_LAB8\bin\Debug\net8.0";
+                string searchfile = "animal.xml";
+                string[] files = Directory.GetFiles(filePath, searchfile, SearchOption.AllDirectories);
+                if (files.Length > 0)
+                {
+                    using (FileStream fileStream = File.OpenRead(files[0])) //поиск файла и вывод его содержимого на консоль
+                    {
+                        byte[] buffer = new byte[fileStream.Length];
+                        fileStream.Read(buffer, 0, buffer.Length);
+                        Console.WriteLine(System.Text.Encoding.Default.GetString(buffer));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл не найден");
+                }
+
+                string fileName = @"C:\Users\Pavel\source\repos\C#_LAB8\bin\Debug\net8.0\animal.xml";
+                string compressedFilePath = $"{fileName}.gz";
+                using (FileStream sourceFile = File.OpenRead(fileName)) //сжатие файла
+                {
+                    using (FileStream targetFile = File.Create(compressedFilePath))
+                    {
+                        using (var compressor = new System.IO.Compression.GZipStream(targetFile, System.IO.Compression.CompressionMode.Compress))
+                        {
+                            sourceFile.CopyTo(compressor);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
